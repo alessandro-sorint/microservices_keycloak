3b299eeeabca561556d06f790ab7c25caadc552a
alessandro-sorint <68538923+alessandro-sorint@users.noreply.github.com>
2020-09-15 12:54:30 +0200

    Add files via upload

---
+++ discovery-service/src/main/java/com/example/discoveryservice/EurekaServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.discoveryservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+		//new SpringApplicationBuilder(EurekaServiceApplication.class).web(WebApplicationType.SERVLET).run(args);
+	}
+
+}
