abc7240956e599b5519e4041a65ffee21c86d8f5
alessandro-sorint <68538923+alessandro-sorint@users.noreply.github.com>
2020-09-15 12:52:15 +0200

    Add files via upload

---
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,113 @@
+package com.example.apigateway;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import lombok.Data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+//https://spring.io/guides/gs/routing-and-filtering/
+
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
+
+@Data
+class Car {
+    private String name;
+    private Long id;
+    
+    public Long getId() {
+    	return id;
+    }
+
+	public String getName() {
+		return name;
+	}
+}
+
+@FeignClient("car-service")
+interface CarClient {
+	
+    @GetMapping("/cars")
+    @CrossOrigin
+    CollectionModel<Car> readCars();
+}
+
+@RestController
+class CoolCarController {
+
+    private final CarClient carClient;
+
+    public CoolCarController(CarClient carClient) {
+        this.carClient = carClient;
+    }
+
+    private Collection<Car> fallback() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping("/cool-cars")
+    @CrossOrigin
+    @HystrixCommand(fallbackMethod = "fallback")
+    public Collection<Car> goodCars() {
+        return carClient.readCars()
+                .getContent()
+                .stream()
+                .filter(this::isCool)
+                .collect(Collectors.toList());
+    }
+    
+    private boolean isCool(Car car) {
+        return !car.getName().equals("AMC Gremlin") &&
+                !car.getName().equals("Triumph Stag") &&
+                !car.getName().equals("Ford Pinto") &&
+                !car.getName().equals("Yugo GV");
+    }
+}
